48d1dd247516c082baf300ecf6e903bd59e0e6fa
Michael <kllmmc23@gmail.com>
2020-08-30 22:06:12 -0600

    repo2

--- js-ecommerce/frontend/src/screen/HomeScreen.js
+++ js-ecommerce/frontend/src/screen/HomeScreen.js
@@ -1,12 +1,12 @@
 import axios from "axios";
+import { APIINITIAL } from "../config.js";
 
 const HomeScreen = {
   render: async () => {
 
     let response = await axios({
       //"http://api.searchspring.net/api/search/search.json?siteId=scmq7n&q=jeans&resultsFormat=native&page=2"
-      url:
-        "http://api.searchspring.net/api/search/search.json?ajaxCatalog=v3&resultsFormat=native&siteId=scmq7n&domain=http%3A%2F%2Ftry.searchspring.com%2Fnew-arrivals&q=&userId=V3-4C313EC5-FE8F-4384-9F35-59C99F2BDD47&tracking=true&disableRedirects=1&bgfilter.days_since_published.low=*&bgfilter.days_since_published.high=30&collateDym=0",
+      url:`${APIINITIAL}`,
       headers: { "Content-Type": "application/json" },
     });
     if (!response || response.statusText !== 'OK') {
